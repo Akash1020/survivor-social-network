f316dd53ee8374638fccfae5b5ee221e6f53a5f4
Itair Miguel <itair.jr@gmail.com>
2017-04-13 00:35:30 -0300

    Initial Commit

---
+++ src/test/java/org/zombie/apocalipse/api/reports/SurvivorReportsControllerITCase.java
@@ -0,0 +1,65 @@
+package org.zombie.apocalipse.api.reports;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.zombie.apocalipse.api.ZombieApocalipseApiApplicationTests;
+
+public class SurvivorReportsControllerITCase extends ZombieApocalipseApiApplicationTests {
+	
+	private static final String BASE_URL = "/v1/app/reports/";
+	
+	@Test
+	public void shouldReturnPercentageOfInfectedSurvivors() throws Exception {
+		MockHttpServletRequestBuilder requestBuilder = get(BASE_URL + "infected");
+
+		super.perform(requestBuilder)
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.percentage").value(9.0))
+				.andDo(print());
+	}
+	
+	@Test
+	public void shouldReturnPercentageOfNonInfectedSurvivors() throws Exception {
+		MockHttpServletRequestBuilder requestBuilder = get(BASE_URL + "not_infected");
+
+		super.perform(requestBuilder)
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.percentage").value(91.0))
+				.andDo(print());
+	}
+	
+	@Test
+	public void shouldReturnAverageAmounmtOfResourcesPerSurvivor() throws Exception {
+		MockHttpServletRequestBuilder requestBuilder = get(BASE_URL + "inventory");
+
+		super.perform(requestBuilder)
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.waterPerSurvivor").value(5.0))
+				.andExpect(jsonPath("$.data.ammunitionPerSurvivor").value(4.0989010989010985))
+				.andExpect(jsonPath("$.data.foodPerSurvivor").value(4.516483516483516))
+				.andExpect(jsonPath("$.data.medicationPerSurvivor").value(4.824175824175824))
+				.andDo(print());
+	}
+	
+	@Test
+	public void shouldReturnPointsLostDueToInfectedSurvivors() throws Exception {
+		MockHttpServletRequestBuilder requestBuilder = get(BASE_URL + "points_lost");
+
+		super.perform(requestBuilder)
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.pointsLost").value(361))
+				.andDo(print());
+	}
+	
+}
